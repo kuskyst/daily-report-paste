522cbe74e65bf006323371fac5105e01e0fd9e5f
kuskyst <126965999+kuskyst@users.noreply.github.com>
2024-02-15 10:59:23 +0900

    Update background.js

--- src/background.js
+++ src/background.js
@@ -1,10 +1,10 @@
 const onClick = () => (_, tab) => chrome.tabs.sendMessage(tab.id, '');
 chrome.runtime.onInstalled.addListener(function() {
   chrome.contextMenus.create({
-    title : 'Daily Report Paste',
+    title : 'Daily Report Paster',
     type : 'normal',
     contexts : ['all'],
-    id: 'daily-report-paste',
+    id: 'daily-report-paster',
     onclick : onClick()
   })
 })
